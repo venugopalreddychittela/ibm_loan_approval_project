f9026a12dafdc5687bc183cf26c2bf70cdcf9c0d
venugopalreddychittela <venugopalreddychittela@gmail.com>
2026-08-15 21:17:41 +0530

    Finalize application for deployment

--- app.py
+++ app.py
@@ -6,7 +6,6 @@ import os
 import sqlite3
 import hashlib
 from datetime import datetime
-st.write("RUNNING NEW APP.PY")
 
 # ============================================================
 # PAGE CONFIGURATION
